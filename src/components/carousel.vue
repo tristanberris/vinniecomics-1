878506a8abbc969255072e0a1b4d6b10c22251f4
coding-tristanberris <coding.berris.tristan@gmail.com>
2020-10-04 20:15:14 -0600

    removing images due to URL expiration

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -12,21 +12,21 @@
             <div class="carousel-item active">
               <img
                 class="d-block w-100 img-fluid car-image"
-                src="https://instagram.fboi1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/116215947_145794583821962_2686294316445970039_n.jpg?_nc_ht=instagram.fboi1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=uJfYGEPUJeAAX8Q8aJm&oh=1b60b7e1fd45112098379bf605020bb9&oe=5F553565"
+                src=""
                 alt="First slide"
               />
             </div>
             <div class="carousel-item">
               <img
                 class="d-block w-100 car-image"
-                src="https://instagram.fboi1-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/96173059_2311168605858335_4094833614201353109_n.jpg?_nc_ht=instagram.fboi1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=TZaAJiGAhMwAX9S_YA4&oh=13fac57e7eabddc001ed9ad2ddee1881&oe=5F52090B"
+                src=""
                 alt="Second slide"
               />
             </div>
             <div class="carousel-item">
               <img
                 class="d-block w-100 car-image"
-                src="https://instagram.fboi1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/116558351_923645838133901_6093552401345993248_n.jpg?_nc_ht=instagram.fboi1-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=nSHqPHAENGYAX9dO0Iy&oh=f7b89f9ab92da96bc7eacf009f103948&oe=5F547178"
+                src=""
                 alt="Third slide"
               />
             </div>
